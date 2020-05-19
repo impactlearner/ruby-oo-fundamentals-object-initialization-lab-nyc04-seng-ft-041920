7215986605095dd1fc0cbe1d4bd9eac490d3f88e
impact learner <time.darden@gmail.com>
2020-05-19 13:34:42 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+  def initialize(name, breed)
+    @name = name
+
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  # def breed=(breed)
+  #   @breed = breed
+  # end
+
+  def name
+    @name
+  end
+
+  # def breed
+  #   @breed
+  # end
+end
+
+mutt = Dog.new
+mutt.name = "Mutt"
